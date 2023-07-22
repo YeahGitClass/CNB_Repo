15c2aff56a1df03e48dc4a37dcd98558671c517e
YeahGitClass <narayananseight@gmail.com>
2023-07-22 22:21:07 +0530

    commiting priority task

--- Demo.java
+++ Demo.java
@@ -1,3 +1,11 @@
 class Demo{
   Testing pull and conflict by changing two different files
 }
+
+//task 120 -- start
+
+task 101 save to temporary memory by using git stash.
+once you complete task 120 and push the changes to git repo you can restore that incomplete code 
+by using git stash apply
+
+//task 120 -- end
--- Wish.java
+++ Wish.java
@@ -1,3 +1,11 @@
 class Wish{
 	modifying in local repo
 }
+
+
+//task 120 -- start
+
+task 101 save to temporary memory by using git stash.
+once you complete task 120 and push the changes to git repo you can restore that incomplete code 
+by using git stash apply
+//task 120 -- end
