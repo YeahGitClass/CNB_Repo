e308e5c6172454988e2be1a515df118442acee29
YeahGitClass <narayananseight@gmail.com>
2023-07-22 22:30:17 +0530

    git stash apply example

--- Greet.java
+++ Greet.java
@@ -1,5 +1,6 @@
 This is modified in local repo
 
+<<<<<<< Updated upstream
 //task 120 -- start
 
 task 101 save to temporary memory by using git stash.
@@ -7,3 +8,16 @@ once you complete task 120 and push the changes to git repo you can restore that
 by using git stash apply
 
 //task 120 -- end
+=======
+//task 101 start
+
+hello testing git stash command usage
+git stash will be used when a developer forced to stop the present work and advised to work on
+other task which is having priority.  In that situation, instead of undo the present work we can
+save that work in temporary storage/location/memory.  once the priority work is completed and 
+pushed to git repository, we can recall/restore the code/work which was stopped in middle.
+for that we can use git stash apply command.
+
+//task 101 end
+
+>>>>>>> Stashed changes
--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,7 @@ This is in remote repo
 This is from local repo
 }
 
+<<<<<<< Updated upstream
 //task 120 -- start
 
 task 101 save to temporary memory by using git stash.
@@ -10,3 +11,15 @@ once you complete task 120 and push the changes to git repo you can restore that
 by using git stash apply
 
 //task 120 -- end
+=======
+//task 101 start
+
+hello testing git stash command usage
+git stash will be used when a developer forced to stop the present work and advised to work on
+other task which is having priority.  In that situation, instead of undo the present work we can
+save that work in temporary storage/location/memory.  once the priority work is completed and 
+pushed to git repository, we can recall/restore the code/work which was stopped in middle.
+for that we can use git stash apply command.
+
+//task 101 end
+>>>>>>> Stashed changes
