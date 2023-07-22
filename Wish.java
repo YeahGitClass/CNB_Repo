561a80c82e0fd3e0306673bba18a16db020e2783
YeahGitClass <narayananseight@gmail.com>
2023-07-22 22:24:10 +0530

    task 101

--- Wish.java
+++ Wish.java
@@ -8,4 +8,5 @@ class Wish{
 task 101 save to temporary memory by using git stash.
 once you complete task 120 and push the changes to git repo you can restore that incomplete code 
 by using git stash apply
+
 //task 120 -- end
