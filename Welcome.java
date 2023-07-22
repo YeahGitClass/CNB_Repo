6fbbdf9cfeb38854229c435caa89350dc8f5b228
YeahGitClass <narayananseight@gmail.com>
2023-07-22 22:28:03 +0530

    git stash example

--- Welcome.java
+++ Welcome.java
@@ -2,3 +2,11 @@ class Welcome{
 This is in remote repo	
 This is from local repo
 }
+
+//task 120 -- start
+
+task 101 save to temporary memory by using git stash.
+once you complete task 120 and push the changes to git repo you can restore that incomplete code 
+by using git stash apply
+
+//task 120 -- end
